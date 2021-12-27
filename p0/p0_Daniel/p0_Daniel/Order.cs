8285fd0cd7fdd9bacc88f4a757e801adf6836ed6
Daniel Spillers <spillersdaniela@gmail.com>
2021-12-26 23:04:50 -0500

    project done

--- p0/p0_Daniel/p0_Daniel/Order.cs
+++ p0/p0_Daniel/p0_Daniel/Order.cs
@@ -8,7 +8,6 @@ namespace p0_Daniel
 {
     public class Order
     {
-        public Guid OrderId { get; set; }
         public int productId { get; set; }
         public string productName { get; set; }
         public string description { get; set; }
