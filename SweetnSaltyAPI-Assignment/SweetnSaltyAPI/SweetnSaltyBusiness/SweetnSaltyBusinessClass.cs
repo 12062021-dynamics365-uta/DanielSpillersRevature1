a6e9f61fffc90a5675f01980e13c781799c11616
Daniel Spillers <spillersdaniela@gmail.com>
2022-01-07 18:29:08 -0500

    uploading changes

---
+++ SweetnSaltyAPI-Assignment/SweetnSaltyAPI/SweetnSaltyBusiness/SweetnSaltyBusinessClass.cs
@@ -0,0 +1,26 @@
+﻿using SweetnSaltyDbAccess;
+using SweetnSaltyModels;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SweetnSaltyBusiness
+{
+    public class SweetnSaltyBusinessClass : ISweetnSaltyBusinessClass
+    {
+        private readonly ISweetnSaltyDbAccessClass _dbAccess;
+        public SweetnSaltyBusinessClass(ISweetnSaltyDbAccessClass Dbaccess)//you need a reference to the DbAccess Layer 
+        {
+            this._dbAccess = Dbaccess;
+        }
+
+        public Task<Flavor> PostFlavor(string flavor)
+        {
+            SqlDataReader dr = this._dbAccess.PostFlavor();
+            if (dr.Read())
+            {
+
+            }
+        }
+    }
+}
